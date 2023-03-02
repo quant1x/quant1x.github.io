c8ad700c92f03decc7927e2127f5aa0321eb50de
wangfeng <wangfengxy@sina.cn>
2023-03-02 10:30:20 +0800

    更新版本 1.3.5

--- publish.sh
+++ publish.sh
@@ -99,8 +99,10 @@ fi
 sed "s/\${quant_version}/${quantVersion}/g" index.tpl| sed "s/\${data_version}/${dataVersion}/g" > index.html
 git add .
 git commit -m "更新版本 ${quantVersion}"
+git tag --delete v${quantVersion}
 git tag -a v${quantVersion} -m "Release version ${quantVersion}"
 git push
+git push origin :v${quantVersion}
 git push --tags
 cd $p0
 
